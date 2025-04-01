e5dce137ce72ac144d751529f466087c77735acf
umar ahmed <umarahmedahmed24@gmail.com>
2025-04-02 00:59:16 +0530

    1new commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ padding: '1rem', background: '#282c34', color: 'white', textAlign: 'center' }}>
+      &copy; 2025 LevelUp. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
